3d0e95ec581e7c8a3f9cfcb873facf98536528c5
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-12-20 16:12:31 +0700

    create Model

---
+++ lib/models/catgegory.dart
@@ -0,0 +1,5 @@
+class Category {
+  late String category;
+
+  Category(this.category);
+}
---
+++ lib/models/result.dart
@@ -0,0 +1,15 @@
+class Product {
+  late String id;
+  late String resultscan;
+  late String name;
+  late String category;
+  late DateTime dateAdded;
+
+  Product(
+    this.id,
+    this.resultscan,
+    this.name,
+    this.category,
+    this.dateAdded
+  );
+}
