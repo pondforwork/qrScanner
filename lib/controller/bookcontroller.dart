04593c247101c63c0a6e9e65f7dde590a990ce1c
pondzaa114 <pharadonsiriforwork@gmail.com>
2024-01-31 20:00:39 +0700

    Add path

--- lib/controller/bookcontroller.dart
+++ lib/controller/bookcontroller.dart
@@ -1,9 +1,9 @@
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class BookController extends GetxController {
+  var db = await openDatabase('my_db.db');
 
 }
 
-void searchAll() {
-  
-}
