8ee99232d8d1fef93ad9ce676fef6fc0334fb970
pondzaa114 <pharadonsiriforwork@gmail.com>
2024-01-31 19:53:14 +0700

    Add textfield

--- lib/screen/searchbookview.dart
+++ lib/screen/searchbookview.dart
@@ -4,7 +4,8 @@ import 'package:qr_scan/controller/categorycontroller.dart';
 import 'package:qr_scan/controller/productcontroller.dart';
 
 class SearchBookView extends StatelessWidget {
-  const SearchBookView({Key? key});
+  const SearchBookView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Get.put(CategoryController());
@@ -14,47 +15,41 @@ class SearchBookView extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Search Book"),
       ),
-      body: GetX<ProductController>(
-        builder: (controller) {
-          if (controller.allProduct.isEmpty) {
-            // Display a message when there are no products
-
-            return const Center(
-              child: Text("No Product",style: TextStyle(fontSize: 20),),
-            );
-          } else {
-            // Display the list of products when there are items
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: controller.allProduct.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 80,
-                        child: Card(
-                          child: Row(children: [Center(
-                            child: Text(
-                              controller.allProduct[index].name + " ID  ",
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              controller.allProduct[index].resultscan,
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          ),
-                          ],)
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            SizedBox(
+              height: 300,
+            ),
+            Expanded(
+              child: TextField(
+                // controller: _controller,
+                onSubmitted: (String value) async {
+                  await showDialog<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Thanks!'),
+                        content: Text(
+                          'You typed "$value", which has length ${value.length}.',
                         ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
                       );
                     },
-                  ),
-                ),
-              ],
-            );
-          }
-        },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
