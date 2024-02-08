9ba913b8467c9851258f9ed162527cfaebbf0333
pondzaa114 <pharadonsiriforwork@gmail.com>
2024-02-08 20:36:11 +0700

    Try update

--- lib/screen/loginview.dart
+++ lib/screen/loginview.dart
@@ -1,28 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:qr_scan/screen/navbar.dart';
 import 'package:qr_scan/screen/scanview.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({Key? key});
+class LoginView extends StatefulWidget {
+  LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final Future<FirebaseApp> _firebase;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeFirebase();
+  }
+   Future<FirebaseApp> initializeFirebase() async {
+    try {
+      return await Firebase.initializeApp();
+    } catch (e) {
+      print('Error initializing Firebase: $e');
+    throw e; // Rethrow the error to handle it at a higher level if needed
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    //call category to load dropdown first
-    // ignore: unused_local_variable
-    // Now you can use dropdownController.dropdownItems in your UI to populate the dropdown.
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(child: SizedBox(child: Text("เข้าสู่ระบบ",style: TextStyle(fontSize: 35),)),),
-          SizedBox(height: 100,),
+          Center(
+            child: SizedBox(
+              child: Text(
+                "เข้าสู่ระบบ",
+                style: TextStyle(fontSize: 35),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
           Center(
             child: SizedBox(
               width: 350,
               child: Padding(
                 padding: const EdgeInsets.all(0),
                 child: ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
+                    await loginEmailAndPassword();
                     print("Test");
                   },
                   style: ElevatedButton.styleFrom(
@@ -36,9 +67,9 @@ class LoginView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(
-                        'assets/images/Google_Icon.png', 
-                        width: 50, 
-                        height: 50, 
+                        'assets/images/Google_Icon.png',
+                        width: 50,
+                        height: 50,
                       ),
                       const SizedBox(width: 8),
                       const Text(
@@ -56,5 +87,19 @@ class LoginView extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> loginEmailAndPassword() async {
+    try {
+      await _firebase; // Ensure Firebase is initialized
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: "ponduuu123@gmail.com",
+        password: "Pxnd124",
+      );
+      // Navigate to the next screen or perform other actions upon successful login
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
+      // Handle login error (show message to user, etc.)
+    }
   }
 }
