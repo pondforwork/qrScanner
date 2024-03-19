534f9e94f6df52fed6fc3eecabba95a5d7863fe8
pharadon sirijhan <pharadonsiriforwork@gmail.com>
2024-03-19 18:45:14 +0700

    ScanContinuous

--- lib/screen/scanview.dart
+++ lib/screen/scanview.dart
@@ -261,9 +261,14 @@ class Scanview extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
+          print("Working");
           await scannercontroller.scanandsearchFromDB();
-          // await scannercontroller.scanContinuous();
-          // print(scannercontroller.barcode.value);
+          while (scannercontroller.scan.value == true) {
+            print("Delaying");
+            await Future.delayed(Duration(seconds: 3));
+            print("Running");
+            await scannercontroller.scanandsearchFromDB();
+          }
         },
         child: const Icon(Icons.qr_code_2_outlined),
       ),
